d7f38bf75b270a8bd042d42220ceb19fb3076f4a
Kyle Holzinger <Kyle@Kyles-MacBook-Pro.local>
2015-10-10 17:06:03 -0400

    [KH] Angular app mostly working

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,18 +1,68 @@
-require("angular");
+// require("angular");
+// require("angular-ui-bootstrap");
 
-var app = angular.module("RubberTracks", []);
+var app = angular.module("RubberTracks", ["ui.bootstrap"]);
 
 app.controller("MainController", function ($scope, $http) {
+	$scope.changeFilter = function (newFilter) {
+		$scope.currentFilter = newFilter;
+	};
+	$scope.segments = {};
+	$scope.filterList = ["genre"];
+	$scope.filters = {};
+	$scope.currentFilter = "genres";
+
 	console.log("Controller initialized");
 	$http(
 		{
 			method: "GET",
-			url: "https://hackathon.indabamusic.com/samples"
+			url: "/filters"
+		}
+		).then(function success(response) {
+			$scope.filterList = Object.keys(response.data).map(function (el) {
+				return el.replace(/_/g, " ");
+			});
+			$scope.filters = response.data;
+			console.log($scope.filters, $scope.currentFilter);
+			console.log("fetched keys");
+			$scope.filterList.forEach(function (el) {
+				$http({
+					method: "GET",
+					url: "/count/" + el;
+				}).then(
+				function success (res) {
+					console.log(res);
+				},
+				function err (res) {
+					console.log("Cannot fetch count for", el);
+				});
+			});
+		}, function failure(response) {
+			console.log("Error loading");
+		}
+	);
+
+	$http(
+		{
+			method: "GET",
+			url: "/samples"
 		}
 	).then(function success(response) {
-		console.log(response.data);
-
+		console.log("Fetched data...");
+		var data = response.data;
+		for (var i = 0; i < data.length; i++) {
+			if (data[i].genres) {
+				data[i].genres.forEach(function (el) {
+					$scope.segments[el] = $scope.segments[el] + 1 || 1;
+				});
+			}
+		}
+		console.log("GENRES:", $scope.segments);
+		$scope.genreList = Object.keys($scope.segments).map(function (el) {
+			return {name: el, count: $scope.segments[el]};
+		});
 	}, function failure(response) {
 		console.log(response);
 	});
+
 });
--- server/index.js
+++ server/index.js
@@ -2,9 +2,53 @@ var express = require("express");
 var app = express();
 
 var request = require("request");
+var async = require("async");
+var compression = require("compression");
+
+var samples;
+var filters;
+
+async.parallel([
+	function (cb) {
+		request("https://hackathon.indabamusic.com/samples", function (req, res) {
+			samples = JSON.parse(res.body);
+			console.log("Loaded samples");
+			cb();
+		});
+	},
+	function (cb) {
+		request("http://hackathon.indabamusic.com/samples?filters_only=true", function (req, res) {
+			filters = JSON.parse(res.body);
+			console.log("Loaded filters");
+			cb();
+		});
+	}
+],
+function () {
+	console.log("Loaded all data");
+});
+
+app.use(compression());
+
+app.get("/samples", function (req, res) {
+	if (samples) {
+		res.json(samples);
+	} else {
+		res.status(503).send({error: "data not loaded yet"});
+	}
+});
+
+app.get("/filters", function (req, res) {
+	if (filters) {
+		res.json(filters);
+	} else {
+		res.status(503).send({error: "data not loaded yet"});
+	}
+})
 
 app.use(express.static("./"))
 app.use(express.static("public"));
+
 
 app.listen(3000, function () {
     console.log("Listening at localhost:3000...");
